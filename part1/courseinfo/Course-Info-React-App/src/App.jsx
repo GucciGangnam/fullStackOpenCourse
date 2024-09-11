ef405583d8cb400c121e343c8e737478a6f37d4f
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-11 16:02:16 +0700

    Part 1.6 complete

--- part1/courseinfo/Course-Info-React-App/src/App.jsx
+++ part1/courseinfo/Course-Info-React-App/src/App.jsx
@@ -1,39 +1,39 @@
-// IMPIRTDS 
+import { useState } from 'react'
 // Styles 
 import "./App.css"
 
-// Components 
-import { Header } from "./components/Header"
-import { Content } from "./components/Content"
-import { Total } from "./components/Total"
-
-
 const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  // BUtton Handlers 
+  const handleIncreaseGood = () => { 
+    setGood((prev) => (prev + 1))
+      
+  }
+  const handleIncreaseNeutral = () => { 
+    setNeutral((prev) => (prev + 1))
+      
+  }
+  const handleIncreaseBad = () => { 
+    setBad((prev) => (prev + 1))
+      
   }
 
-
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+
+    <div className='App'>
+      <button onClick={handleIncreaseGood}> Good </button>
+      <button onClick={handleIncreaseNeutral}> Neutral </button>
+      <button onClick={handleIncreaseBad}> Bad </button>
+
+      <div className='Results'>
+        <h1> Statiustics </h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
     </div>
   )
 }
