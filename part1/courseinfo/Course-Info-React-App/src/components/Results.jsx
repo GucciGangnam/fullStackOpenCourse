186e71f34eb629372a043578860ca2273d815ab8
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-11 16:22:41 +0700

    Part 1.9 complete

--- part1/courseinfo/Course-Info-React-App/src/components/Results.jsx
+++ part1/courseinfo/Course-Info-React-App/src/components/Results.jsx
@@ -5,19 +5,26 @@
 
 export const Results = ({ good, neutral, bad }) => {
     return (
-        <div className="Results">
-            <div className='Results'>
-                <h1> Votes </h1>
-                <p>Good: {good}</p>
-                <p>Neutral: {neutral}</p>
-                <p>Bad: {bad}</p>
 
-                <h1> Statistics </h1>
-                <p>Total Votes: {good + neutral + bad}</p>
-                <p>Average: {((good - bad) / (good + neutral + bad || 1)).toFixed(2)}</p>
-                <p>Positive percent: {(good / (good + neutral + bad || 1) * 100).toFixed(2)} %</p>
-
-            </div>
+        <div className='Results'>
+            {(good === 0) && (neutral === 0) && (bad === 0) ? (
+                <>
+                    <p>
+                        No feedback given.  Click a button to submit feedback.
+                    </p>
+                </>
+            ) : (
+                <>
+                    <h1> Votes </h1>
+                    <p>Good: {good}</p>
+                    <p>Neutral: {neutral}</p>
+                    <p>Bad: {bad}</p>
+                    <h1> Statistics </h1>
+                    <p>Total Votes: {good + neutral + bad}</p>
+                    <p>Average: {((good - bad) / (good + neutral + bad || 1)).toFixed(2)}</p>
+                    <p>Positive percent: {(good / (good + neutral + bad || 1) * 100).toFixed(2)} %</p>
+                </>
+            )}
         </div>
     )
 }
