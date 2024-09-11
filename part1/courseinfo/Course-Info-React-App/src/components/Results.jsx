982835dec46a44cfde58343b3816e8a574e2416a
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-11 16:35:14 +0700

    Part 1.11 complete - Part 1.10 skipped and all Result data is kept within 1 component

--- part1/courseinfo/Course-Info-React-App/src/components/Results.jsx
+++ part1/courseinfo/Course-Info-React-App/src/components/Results.jsx
@@ -15,14 +15,41 @@ export const Results = ({ good, neutral, bad }) => {
                 </>
             ) : (
                 <>
-                    <h1> Votes </h1>
-                    <p>Good: {good}</p>
-                    <p>Neutral: {neutral}</p>
-                    <p>Bad: {bad}</p>
-                    <h1> Statistics </h1>
-                    <p>Total Votes: {good + neutral + bad}</p>
-                    <p>Average: {((good - bad) / (good + neutral + bad || 1)).toFixed(2)}</p>
-                    <p>Positive percent: {(good / (good + neutral + bad || 1) * 100).toFixed(2)} %</p>
+                    <h1>Votes</h1>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Good:</td>
+                                <td>{good}</td>
+                            </tr>
+                            <tr>
+                                <td>Neutral:</td>
+                                <td>{neutral}</td>
+                            </tr>
+                            <tr>
+                                <td>Bad:</td>
+                                <td>{bad}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <h1>Statistics</h1>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Total Votes:</td>
+                                <td>{good + neutral + bad}</td>
+                            </tr>
+                            <tr>
+                                <td>Average:</td>
+                                <td>{((good - bad) / (good + neutral + bad || 1)).toFixed(2)}</td>
+                            </tr>
+                            <tr>
+                                <td>Positive Percent:</td>
+                                <td>{(good / (good + neutral + bad || 1) * 100).toFixed(2)} %</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </>
             )}
         </div>
