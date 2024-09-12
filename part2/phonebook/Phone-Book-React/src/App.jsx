79270cc022d7112c650068ddd1153f358e917b9a
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-12 14:05:05 +0700

    Part 2.6 complete

---
+++ part2/phonebook/Phone-Book-React/src/App.jsx
@@ -0,0 +1,53 @@
+
+// IMPORTS 
+// Styles 
+import "./App.css"
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  // Handle change name 
+  const handleChangename = (e) => {
+    setNewName(e.target.value)
+  }
+
+  // SUbmit new name 
+  const submitName = (e) => {
+    e.preventDefault();
+    const updatedPersons = [...persons, { name: newName }]
+    setPersons(updatedPersons)
+    setNewName('')
+  }
+
+  return (
+    <div className='App'>
+      <h2>Phonebook</h2>
+
+      <form
+        onSubmit={submitName}>
+        <div>
+          name: <input
+            placeholder="Type name here"
+            value={newName}
+            onChange={handleChangename} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export default App
