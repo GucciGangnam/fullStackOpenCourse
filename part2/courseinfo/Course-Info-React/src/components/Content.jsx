c86c0964480e2f06846fe9b6d95e14183f9a3536
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-12 12:39:23 +0700

    Part 2.2 AND 2.3 complete

--- part2/courseinfo/Course-Info-React/src/components/Content.jsx
+++ part2/courseinfo/Course-Info-React/src/components/Content.jsx
@@ -6,15 +6,18 @@ import { Part } from "./part"
 // COMPONENT
 
 export const Content = ({ course }) => {
+
+
+    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
     return (
         <div className="Content">
-
-
 
             {course.parts.map((part) => (
                 <Part key={part.id} part={part} />
             ))}
 
+            {total}
 
         </div>
     )
