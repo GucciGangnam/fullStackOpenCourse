8473657d7c11e6f17400020855c6cf645c6e12a4
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-17 12:49:58 +0700

    Part 2.20 complete

--- part2/countries/Countries-React/src/componenets/Results.jsx
+++ part2/countries/Countries-React/src/componenets/Results.jsx
@@ -1,9 +1,32 @@
 // IMPORTS 
-
-
+// React 
+import { useState, useEffect } from "react";
+import axios from "axios";
+// Variables 
+const api_key = import.meta.env.VITE_WEATHER_API_KEY
 
 // COMPONENET 
 export const Results = ({ searchResults, selectCountry }) => {
+
+    const [onlyOneResult, setOnlyOneResult] = useState(false);
+
+    const [weatherInfo, setWeatherInfo] = useState({});
+
+    // UE to update OOR to tru when 1 result and F when not 
+
+    useEffect(() => {
+        if (searchResults.length === 1) {
+            setOnlyOneResult(true);
+            axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${searchResults[0].name.common}&aqi=no`)
+                .then(result => setWeatherInfo(result.data))
+
+        } else {
+            setOnlyOneResult(false);
+            setWeatherInfo({})
+        }
+    }, [searchResults])
+
+
     return (
         <div className="Results">
 
@@ -34,6 +57,13 @@ export const Results = ({ searchResults, selectCountry }) => {
                             ))}
                         </ul>
                     </div>
+
+                    <div className="Weather">
+                        <p>Weather in {searchResults[0].capital}</p>
+                        <p>  {weatherInfo?.current?.condition?.text || 'Weather information not available'}</p>
+                        <p>  {weatherInfo?.current?.temp_c || 'Weather information not available'} c</p>
+                        <img src={weatherInfo?.current?.condition?.icon}/> 
+                    </div>
                 </div>
             ) : (searchResults.length > 249) ? (
                 <p>Results will be displayed here</p>
@@ -42,7 +72,7 @@ export const Results = ({ searchResults, selectCountry }) => {
                     <div key={country.name.common} className="Country">
                         {country.name.common}
                         <button
-                        onClick={() => {selectCountry(country.name.common)}}>Show</button>
+                            onClick={() => { selectCountry(country.name.common) }}>Show</button>
                     </div>
                 ))
             ) : (
