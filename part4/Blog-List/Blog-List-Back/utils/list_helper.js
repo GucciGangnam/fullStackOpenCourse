119882a1d82294d54a9baf79ccbf05299167dec1
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-23 12:14:53 +0700

    Part 4.5 complete

--- part4/Blog-List/Blog-List-Back/utils/list_helper.js
+++ part4/Blog-List/Blog-List-Back/utils/list_helper.js
@@ -2,16 +2,19 @@ const dummy = (blogs) => {
     return 1;
 }
 
-const totalLikes = (blogs) => { 
+const totalLikes = (blogs) => {
     let totalLikes = 0
-
-    blogs.forEach((blog) => { 
+    blogs.forEach((blog) => {
         totalLikes += blog.likes;
     })
-
     return totalLikes;
 }
 
+const favouriteBlogs = (blogs) => {
+    if (blogs.length === 0) return 0;
+    return blogs.reduce((max, obj) => obj.likes > max.likes ? obj : max);
+}
+
 module.exports = {
-    dummy, totalLikes 
+    dummy, totalLikes, favouriteBlogs
 }
