20474f9d80285d28beff0b9b8ba893127aab4ea8
Gucci Gangnam <diviner02hive@icloud.com>
2024-09-30 13:46:22 +0700

    Part 5.10 complete

--- part5/Blog-List-Front/src/App.jsx
+++ part5/Blog-List-Front/src/App.jsx
@@ -201,12 +201,15 @@ const App = () => {
 
           <NewBlogForm isCreateFormShowing={isCreateFormShowing} submitNewBlog={submitNewBlog} title={title} handleChangeTitle={handleChangeTitle} url={url} handleChanegURL={handleChanegURL} setIsCreateFormShowing={setIsCreateFormShowing} />
 
-          {blogs.map(blog =>
-            <Blog
-              key={blog.id}
-              blog={blog}
-              setBlogs={setBlogs} />
-          )}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog => (
+              <Blog
+                key={blog.id}
+                blog={blog}
+                setBlogs={setBlogs}
+              />
+            ))}
 
         </div>
 
